f17a2dfdd4f398377af10b8f678a322dfe0bef88
Kill Toxic <913087364@qq.com>
2020-03-20 14:50:42 +0800

    boundingvoxMarking

--- boundingboxMarking/boundingboxMarking.cpp
+++ boundingboxMarking/boundingboxMarking.cpp
@@ -1,20 +1,28 @@
-﻿// boundingboxMarking.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>
+#include<opencv2/opencv.hpp>
 
-#include <iostream>
+using namespace std;
+using namespace cv;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cv::Mat srcMat = cv::imread("D:\\Files\\coins.png");//读取图片
+	cv::Mat binMat;
+	cv::Mat labMat;
+	cv::Mat staMat;
+	cv::Mat cenMat;
+	cv::threshold(srcMat, binMat, 100, 255, THRESH_BINARY);//二值化
+	int num = cv::connectedComponentsWithStats(binMat, labMat, staMat, cenMat,8);//连通域标记
+	for (int i = 1; i < num; i++)//画出连通域
+	{
+		cv::Rect bndbox;
+		bndbox.x = staMat.at<int>(i, 0);
+		bndbox.y = staMat.at<int>(i, 1);
+		bndbox.width = staMat.at<int>(i, 2);
+		bndbox.height = staMat.at<int>(i, 3);
+		cv::rectangle(srcMat, bndbox, CV_RGB(255, 0, 0), 1, 8, 0);
+	}
+	std::cout << "The number of coins is:" << num - 1 << endl;//显示物件个数
+	cv::imshow("srcMat", srcMat);//显示标记连通域后的图像
+	waitKey(0);
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
